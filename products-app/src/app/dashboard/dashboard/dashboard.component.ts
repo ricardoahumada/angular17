65d8ebed8d1af2dd12e7be715572d0a515dd596e
ricardonetmind <ricardoag@expert.netmind.es>
2022-11-18 21:15:10 +0100

    update

--- products-app/src/app/dashboard/dashboard/dashboard.component.ts
+++ products-app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+// import { DynamicComponent } from '../../dynamic/dynamic/dynamic.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  @ViewChild('dynamic', { read: ViewContainerRef })
+  private viewRef: ViewContainerRef={} as ViewContainerRef;
+
+  async loadDynamicComponent() {
+    const { DynamicComponent } = await import("../../dynamic/dynamic/dynamic.component");
+    this.viewRef.clear();
+    this.viewRef.createComponent(DynamicComponent);
   }
 
+  /* showDynamicComponent(): void {
+    this.viewRef.clear();
+    this.viewRef.createComponent(DynamicComponent);
+  }
+ */
+  removeDynamicComponent(): void {
+    this.viewRef.clear();
+  }
 }
