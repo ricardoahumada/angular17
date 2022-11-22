592210c0df9c907ff2be4abc82cac5da0651399a
ricardonetmind <ricardoag@expert.netmind.es>
2022-11-22 12:16:56 +0100

    update

--- products-app/src/app/structure/header/header.component.ts
+++ products-app/src/app/structure/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
+  title:string="Products App"
   constructor() { }
 
   ngOnInit(): void {
