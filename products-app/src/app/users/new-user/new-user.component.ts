bdfa25aa895151b1ba76c18f67f9c4dd91e7c132
ricardonetmind <ricardoag@expert.netmind.es>
2022-11-22 14:08:43 +0100

    update

--- products-app/src/app/users/new-user/new-user.component.ts
+++ products-app/src/app/users/new-user/new-user.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  myform: FormGroup = {} as FormGroup;
 
   ngOnInit(): void {
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: new FormControl('',Validators.minLength(3)),
+        lastName: new FormControl(),
+      }),
+      email: new FormControl(),
+      password: new FormControl(),
+      language: new FormControl(),
+    });
   }
 
 }
