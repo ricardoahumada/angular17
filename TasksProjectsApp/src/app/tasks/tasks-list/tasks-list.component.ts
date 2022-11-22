9f5566d13162806734f422feb8079280288d8e58
ricardonetmind <ricardoag@expert.netmind.es>
2022-11-22 12:17:09 +0100

    update

--- TasksProjectsApp/src/app/tasks/tasks-list/tasks-list.component.ts
+++ TasksProjectsApp/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/models/task';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'tasks-list',
@@ -7,28 +8,25 @@ import { Task } from 'src/app/models/task';
   styleUrls: ['./tasks-list.component.scss'],
 })
 export class TasksListComponent implements OnInit {
-  constructor() {}
+  constructor(private _taskSrv: TasksService) {}
 
-  ngOnInit(): void {}
+  filter_text: string = '';
 
-  filter_text:string='';
+  tasks: Task[] = [];
 
-  tasks: Task[] = [
-    { tid: 1, description: 'Create html', time: 23, project: 1 },
-    { tid: 2, description: 'Create js', time: 23, project: 2 },
-    { tid: 3, description: 'Create TS', time: 23, project: 1 },
-    { tid: 4, description: 'Deploy to production', time: 23, project: 2 },
-    { tid: 5, description: 'Create html', time: 23, project: 1 },
-  ];
+  ngOnInit(): void {
+    this.tasks = this._taskSrv.getTasks();
+  }
 
   deleteTask(tid: number) {
-    this.tasks = this.tasks.filter((aT) => aT.tid != tid);
+    // this.tasks = this.tasks.filter((aT) => aT.tid != tid);
+    this._taskSrv.deleteATask(tid);
   }
 
   show_desc_form: any = {};
 
   showDescForm(tid: number) {
-    this.show_desc_form[  tid] = true;
+    this.show_desc_form[tid] = true;
   }
 
   saveName(text: string, tid: number) {
