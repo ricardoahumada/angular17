5de7ef120ab4175b10296015e582ca1a7879daa7
ricardonetmind <ricardoag@expert.netmind.es>
2022-11-24 11:04:42 +0100

    update

--- products-app/src/app/users/new-user/new-user.component.ts
+++ products-app/src/app/users/new-user/new-user.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { LanguageValidator } from 'src/app/validators/language-validatior';
 
 @Component({
   selector: 'app-new-user',
@@ -7,19 +14,52 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
-  constructor() {}
+  constructor(private _fb: FormBuilder) {}
+
   myform: FormGroup = {} as FormGroup;
+
+  firstName = new FormControl('', [Validators.minLength(3)]);
+  lastName = new FormControl('', Validators.required);
+  email = new FormControl('', [Validators.email, Validators.required]);
 
   ngOnInit(): void {
     this.myform = new FormGroup({
-      name: new FormGroup({
-        firstName: new FormControl('',Validators.minLength(3)),
-        lastName: new FormControl(),
-      }),
-      email: new FormControl(),
+      name: new FormGroup(
+        {
+          firstName: this.firstName,
+          lastName: this.lastName,
+        },
+        Validators.required
+      ),
+      email: this.email,
       password: new FormControl(),
-      language: new FormControl(),
+      language: new FormControl('', LanguageValidator.validateLanguage),
     });
   }
 
+  onSubmit() {
+    console.log('onSubmit:', this.myform);
+    if (this.myform.valid) {
+      //send this to the API
+    } else {
+      //show a message
+    }
+  }
+
+  signUpForm = this._fb.group({
+    name: this._fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(10)]],
+      lastname: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(15),
+          Validators.pattern('^[a-zA-Z]+$'),
+        ],
+      ],
+    }),
+    email: ['', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]],
+    password: ['', [Validators.minLength(8), Validators.required]],
+    language: [''],
+  });
 }
