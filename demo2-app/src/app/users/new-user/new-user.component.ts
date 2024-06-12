f62fc75d7cbde0d5d9b03d45874d247db53d5bb5
ricardo@enmotionvalue.com <ricardo.ahumada>
2024-06-12 12:03:08 +0200

    update

--- demo2-app/src/app/users/new-user/new-user.component.ts
+++ demo2-app/src/app/users/new-user/new-user.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { Signup } from 'src/app/models/signup';
 import { LanguageValidator } from 'src/app/validators/language-validatior';
 
 @Component({
@@ -9,7 +10,8 @@ import { LanguageValidator } from 'src/app/validators/language-validatior';
   styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
-  constructor() { }
+
+  constructor(private _fb: FormBuilder) { }
 
   myForm: FormGroup = {} as FormGroup;
 
@@ -49,6 +51,41 @@ export class NewUserComponent implements OnInit {
     }
   }
 
-  
+
+  /* Using form builder */
+  signUpForm = this._fb.group({
+    name: this._fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(10)]],
+      lastname: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(15),
+          Validators.pattern('^[a-zA-Z]+$'),
+        ],
+      ],
+    }),
+    email: ['', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]],
+    password: ['', [Validators.minLength(8), Validators.required]],
+    language: [''],
+  });
+
+  /* Template driven */
+
+  model: Signup = new Signup();
+
+  @ViewChild('f')
+  sForm: any;
+
+
+  onSubmitTemplate = () => {
+    console.log('onSubmit sForm:', this.sForm, this.model);
+    // revalidar
+    if (this.sForm.valid) {
+      // enviar a API
+      console.log("Envviar model a API....");
+      
+    }
+  }
 
 }
