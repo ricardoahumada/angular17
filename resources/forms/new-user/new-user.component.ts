f35c461e9ece78521733add4dc7fd97fe55aa8f2
ricardoag <ricardo@enmotionvalue.com>
2024-11-12 22:28:58 +0100

    update tutor logic

---
+++ resources/forms/new-user/new-user.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Signup } from 'src/app/models/signup';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.scss'],
+})
+export class NewUserComponent implements OnInit {
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+  }
+
+}
