65665b54001e1fa40b2d94df2973b523a68bdaab
ricardo <ricardo@enmotionvalue.com>
2024-11-13 03:14:17 +0100

    udpate

--- products-app/src/app/products/detail-products/detail-products.component.ts
+++ products-app/src/app/products/detail-products/detail-products.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
-import { Observable, Observer } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-detail-products',
   templateUrl: './detail-products.component.html',
   styleUrl: './detail-products.component.scss'
 })
-export class DetailProductsComponent implements OnInit, OnDestroy {
+export class DetailProductsComponent  {
 
   constructor(private _route: ActivatedRoute, private _productService: ProductService, private _router: Router) {
   }
 
-  pid = 0;
+  /* pid = 0;
   product: Product | undefined = {} as Product;
-  $paramsSubs: any = null;
+   */// $paramsSubs: any = null;
 
+  pidS = toSignal(this._route.params.pipe(map(params => params['pid'])));
+  productS = computed(() => this._productService.getAProduct(this.pidS()));
+  
+/* 
   ngOnInit(): void {
     console.log(this._route.snapshot);
     // this.pid = this._route.snapshot.params['pid'];
@@ -27,7 +32,7 @@ export class DetailProductsComponent implements OnInit, OnDestroy {
       this.pid = data['pid'];
       this.product = this._productService.getAProduct(this.pid);
 
-      if(!this.product) this._router.navigate(['not-found'])
+      if (!this.product) this._router.navigate(['not-found'])
     });
 
   }
@@ -35,6 +40,6 @@ export class DetailProductsComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     console.log('Destuyendo detalle:', this.pid);
     if (this.$paramsSubs) this.$paramsSubs.unsubscribe();
-  }
+  } */
 
 }
