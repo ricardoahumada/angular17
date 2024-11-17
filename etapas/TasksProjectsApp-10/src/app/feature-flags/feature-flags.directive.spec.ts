dd8277d5903aae6ab03858451ef44f715d510de6
ricardoag <ricardo@enmotionvalue.com>
2024-11-17 17:07:29 +0100

    update

---
+++ etapas/TasksProjectsApp-10/src/app/feature-flags/feature-flags.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FeatureFlagsDirective } from './feature-flags.directive';
+
+describe('FeatureFlagsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FeatureFlagsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ etapas/TasksProjectsApp-10/src/app/projects/project-list/project-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { PROJECTS } from '../../data/projects';
+import { ProjectsService } from '../../services/projects.service';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrl: './project-list.component.scss'
+})
+export class ProjectListComponent implements OnInit {
+
+  constructor(private _projectService: ProjectsService) {
+  }
+
+  filter_text: string = '';
+
+  projects: Project[] = [];
+
+  ngOnInit(): void {
+    this.projects = this._projectService.getProjects();
+  }
+}
