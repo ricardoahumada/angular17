5569f815e0bb463f9f9872d8b82d363b9e65afbd
ricardo <ricardo@enmotionvalue.com>
2024-11-20 07:44:58 +0100

    update

--- TasksProjectsApp/src/app/guards/can-activate.guard.ts
+++ TasksProjectsApp/src/app/guards/can-activate.guard.ts
@@ -1,25 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CanActivateGuard implements CanActivate {
-  constructor(private auth: AuthService, protected router: Router){}
+ export class CanActivateGuard implements CanActivate {
+  constructor(private auth: AuthService, protected router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    if(!this.auth.isSignedIn()){
+    if (!this.auth.isSignedIn()) {
       this.router.navigate(['/signin']);
       return false;
-    }else return true;
+    } else return true;
   }
 
 }
 
-/* export const canActivateGuard: CanActivateFn = (route, state) => {
 
+/* export const canActivateGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
 
-  return true;
-};
- */
+  if (!auth.isSignedIn()) {
+    router.navigate(['/signin']);
+    return false;
+  } else return true;
+}; */
